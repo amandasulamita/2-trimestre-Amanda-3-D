96848450e4820c6a6d1e9269c822958d6657927d
amandasulamita <santos.silva.amanda2603@escola.pr.gov.br>
2025-05-30 09:40:34 -0300

    aula3

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = Document.queryselectorAll("botao");
+console (botoes)
+
+for(i=0;i<botoes.length; i++){
+    botoes{i}.onclick = function(){
+        for(j=0; j<botoes.length; j++){
+            botoes[j].classlist.remove(ativo);
+        }
+        botoes[i].classlist.add(ativo);
+    ]
+}
